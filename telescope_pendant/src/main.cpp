7c7bee574f2ed752cea5b2edc7e31e6715af2aaf
EXE167 <thomastruittpowell@gmail.com>
2020-05-10 22:52:03 -0400

    Defined pins for the pendant arduino

--- telescope_pendant/src/main.cpp
+++ telescope_pendant/src/main.cpp
@@ -1,5 +1,22 @@
 #include <Arduino.h>
-
+#include <LiquidCrystal.h>
+#include <Wire.h>
+//pin defs
+  #define txPin 0
+  #define rxPin 1
+  #define lcd1 2
+  #define lcd2 3
+  #define lcd3 4
+  #define lcd4 5
+  #define lcd5 6
+  #define lcd6 7
+  #define enc0 8
+  #define enc1 9
+  #define fxnSw0 10
+  #define fxnSw1 11
+  #define resSw0 12
+  #define resSw1 13
+LiquidCrystal lcd(lcd1, lcd2, lcd3, lcd4, lcd5, lcd6);
 void setup() {
   // put your setup code here, to run once:
 }
